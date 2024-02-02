b98ae1b5dcae7629700a975bec50ee3573751ffa
mujica300 <cesarmujica07@gmail.com>
2024-02-02 10:04:42 -0300

    loginAcces

--- public/login.js
+++ public/login.js
@@ -1,23 +1,4 @@
-<<<<<<< HEAD
-function loguear()
-{
 
-    let user=document.getElementById("email").value;
-    let pass=document.getElementById("password").value;
-
-if(user === "Cesar" && pass === "1234")
-{
-
-    window.location="info.html";
-
-}
-
-else{
-
-    alert("Datos incorrectos");
-}
-
-}
 =======
 const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
