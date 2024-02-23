d4bebce3bb65626cb34fce6df289805e1d8e4eb9
arthemia4 <connierojas63@gmail.com>
2024-02-23 10:16:46 -0300

    vista cursito

--- proyecto-renca/src/Views/MiCursito.jsx
+++ proyecto-renca/src/Views/MiCursito.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/Footer/Footer";
-import MiCurso from "../components/MiCurso1/miCurso";
+import MiCurso from "../components/MiCurso1/MiCurso";
 
 function MiCursito() {
     return (
