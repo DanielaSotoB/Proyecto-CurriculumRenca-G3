3b98509cd4c3a3dc538555776200816a26055939
Janis Aravena <janisaravenaespinoza@gmail.com>
2024-02-29 18:12:51 -0300

    modificaciones modo oscuro en navbar admi, agregar boton perfil  omo incono y ademas modifcar footer

--- proyecto-renca/src/components/Footer/Footer.jsx
+++ proyecto-renca/src/components/Footer/Footer.jsx
@@ -7,23 +7,7 @@ const Footer = () => {
     <>
       <footer className="footer">
         <div className="container">
-          <div className="row">
-            <div className="col-md-2">
-              <img src="../img/logo-renca.png" alt="Logo" className="logo" />
-            </div>
-            <div className="col-md-10 d-flex justify-content-center">
-              <p className="footer-quote text-center">"La educación es el arma más poderosa que puedes usar para cambiar el mundo." - Nelson Mandela</p>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-lg-12">
-              <p className="small text-center text-muted">&copy; 2024 Ruta de Aprendizaje. Todos los derechos reservados.</p>
-            </div>
-          </div>
-        </div>
-      </footer>
-
-      <div className="container py-1">
+          <div className="container py-1">
         <div className="row" style={{ width: '100%', marginLeft: 0, marginBottom: 'margin-down'}}>
           <div className="col-2" style={{ height: '15px', backgroundColor: '#ff571e'}}></div>
           <div className="col-2" style={{ height: '15px', backgroundColor: '#fda22d'}}></div>
@@ -33,9 +17,21 @@ const Footer = () => {
           <div className="col-2" style={{ height: '15px', backgroundColor: '#0ab5fd'}}></div>
         </div>
       </div>
+          <div className="row">
+            <div className="col-md-2">
+              
+              <img src="../img/logo-renca.png" alt="Logo" className="logo" />
+            </div>
+            <div className="col-md-10 d-flex justify-content-center">
+              <p className="h6">"La educación es el arma más poderosa que puedes usar para cambiar el mundo." - Nelson Mandela</p>
+            </div>
+          </div>
+          
+        </div>
+      </footer>
 
       <footer className="w-100 d-flex align-items-center justify-content-center flex-wrap">
-        <p className="fs-5 px-3 pt-3">Currículum Renca. &copy; Todos Los Derechos Reservados 2024</p>
+        <p className="fs-5 px-3 pt-3">Currículum Renca. &copy; 2024</p>
         <div id="iconos">
           <a href="#"><i className="bi bi-facebook"></i></a>
           <a href="#"><i className="bi bi-twitter"></i></a>
--- proyecto-renca/src/components/navbarAdmi/NavbarAdmistrador.jsx
+++ proyecto-renca/src/components/navbarAdmi/NavbarAdmistrador.jsx
@@ -1,18 +1,28 @@
-import React, { useState } from 'react';
-
-
+import React, { useState, useEffect } from 'react';
+import { Dropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const NavbarAdmistrador = () => {
-    // Estado para el modo oscuro
     const [modoOscuro, setModoOscuro] = useState(false);
+    const [showMenu, setShowMenu] = useState(false);
 
-    // Función para cambiar el estado del modo oscuro
     const toggleModoOscuro = () => {
         setModoOscuro(!modoOscuro);
     };
 
+    useEffect(() => {
+        if (modoOscuro) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    }, [modoOscuro]);
+
+    const handleLogoutClick = () => {
+        // Lógica para manejar el clic en "Cerrar sesión"
+    };
+
     return (
-    
         <>
             <header className={`header--featured row mt-4 mb-1 mx-4 d-none d-md-flex ${modoOscuro ? 'dark-mode' : ''}`}>
                 <div className="col-4 col-lg-6">
@@ -29,10 +39,11 @@ const NavbarAdmistrador = () => {
                         <div className="col-6">
                             <div className="row">
                                 <div className="col text-end">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-mask" viewBox="0 0 16 16">
-                                        <path d="M6.225 1.227A7.5 7.5 0 0 1 10.5 8a7.5 7.5 0 0 1-4.275 6.773 7 7 0 1 0 0-13.546M4.187.966a8 8 0 1 1 7.627 14.069A8 8 0 0 1 4.186.964z"/>
-                                    </svg>
-                                    
+                                    <div className="col text-end" onClick={toggleModoOscuro}>
+                                        <svg id="darkIcon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-mask" viewBox="0 0 16 16">
+                                            <path d="M6.225 1.227A7.5 7.5 0 0 1 10.5 8a7.5 7.5 0 0 1-4.275 6.773 7 7 0 1 0 0-13.546M4.187.966a8 8 0 1 1 7.627 14.069A8 8 0 0 1 4.186.964z"/>
+                                        </svg> 
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -62,7 +73,7 @@ const NavbarAdmistrador = () => {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
+                            <li className="nav-item">
                                 <a className="nav-link active" aria-current="page" href="/AdmiPanelView">PANEL ADMINISTRADOR</a>
                             </li>
                             <li className="nav-item dropdown">      
@@ -78,26 +89,25 @@ const NavbarAdmistrador = () => {
                                     <li><a className="dropdown-item" href="/">Cerrar Sesión</a></li>
                                 </ul>
                             </li>
-            
                         </ul>
-                
-                                <a className="nav-link active" aria-current="page" href="/AdmiPanelView">ADMINISTRADOR</a>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="30" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
-                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-                            <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
-                        </svg>
-                        {/* Interruptor del modo oscuro */}
-                        <label className="switch">
-                            <input type="checkbox" id="darkbtn" onChange={toggleModoOscuro} />
-                            <span className="slider round"></span>
-                        </label>
+                        <a className="nav-link active" aria-current="page" href="/AdmiPanelView">ADMINISTRADOR</a>
+                        <Dropdown show={showMenu} onMouseEnter={() => setShowMenu(true)} onMouseLeave={() => setShowMenu(false)}>
+                            <Dropdown.Toggle variant="transparent" id="dropdown-basic">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="30" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16" style={{ cursor: 'pointer' }}>
+                                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                                    <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                                </svg>
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item as={Link} to="/profile">Mi perfil</Dropdown.Item>
+                                <Dropdown.Item onClick={handleLogoutClick}>Cerrar sesión</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
                     </div>
                 </div>
             </nav>
         </>
-       
     );
 }
 
 export default NavbarAdmistrador;
-
