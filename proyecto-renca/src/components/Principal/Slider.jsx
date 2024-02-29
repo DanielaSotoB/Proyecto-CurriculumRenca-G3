d60f8b86dc9c85d1c3da8d82867133df6c66cf59
arthemia4 <connierojas63@gmail.com>
2024-02-29 19:56:25 -0300

    imagen slider

--- proyecto-renca/src/components/Principal/Slider.jsx
+++ proyecto-renca/src/components/Principal/Slider.jsx
@@ -4,15 +4,15 @@ const Principal = () => {
         <div id="carousel" className="carousel slide" data-bs-ride="carousel">
             <div className="carousel-inner">
                 <div className="carousel-item active" data-bs-interval="3000">
-                    <img src="https://codigofacilito.com/system/courses/thumbnails/000/000/340/original/python-thumb.jpg?1635347671" className="d-block w-100" alt="" />
+                    <img src="../public/img/POWER BI BASICO.png" className="d-block w-100" alt="" />
                 </div>
 
                 <div className="carousel-item" data-bs-interval="3000">
-                    <img src="https://pbs.twimg.com/media/FxyVouEXwAE-_Lc.jpg" className="d-block w-100" alt="..." />
+                    <img src="../public/img/PHYTON.png" className="d-block w-100" alt="..." />
                 </div>
 
                 <div className="carousel-item" data-bs-interval="3000">
-                    <img src="https://codigofacilito.com/system/courses/thumbnails/000/000/311/original/Frame_23.png?1637690965" className="d-block w-100" alt="..." />
+                    <img src="../public/img/INGLES.png" className="d-block w-100" alt="..." />
                 </div>
             </div>
             <button className="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
