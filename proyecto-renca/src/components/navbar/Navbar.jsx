9473e2becc76ec5d8ed1d8519d9aca0e61862cae
arthemia4 <connierojas63@gmail.com>
2024-03-05 14:24:03 -0300

    navabar

--- proyecto-renca/src/components/navbar/Navbar.jsx
+++ proyecto-renca/src/components/navbar/Navbar.jsx
@@ -71,7 +71,6 @@ const Navbar = () => {
                                         <path d="M6.225 1.227A7.5 7.5 0 0 1 10.5 8a7.5 7.5 0 0 1-4.275 6.773 7 7 0 1 0 0-13.546M4.187.966a8 8 0 1 1 7.627 14.069A8 8 0 0 1 4.186.964z"/>
                                     </svg> 
                                 </div>
->>>>>>> 7b6e637df951ce8dbc7345b91d6af60bba864ff0
                             </div>
                         </div>
                         <div className="col-6 text-end">
